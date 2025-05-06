d9928ceabd93e2377cb99f20260fa7a8d9edc990
gaurav ashok ghuge <guduughuge7@gmail.com>
2025-05-06 17:32:15 +0530

    allow cookies to the secure oages

--- server/src/app.js
+++ server/src/app.js
@@ -23,41 +23,9 @@ const allowOrigins = [
 ]
 
 app.use(cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-  
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true, // Allow cookies
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Accept",
-      "Origin",
-      "X-Requested-With",
-      "Access-Control-Allow-Origin",
-      "Access-Control-Allow-Credentials",
-      "Access-Control-Allow-Methods",
-      "Access-Control-Allow-Headers"
-    ],
-    exposedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Access-Control-Allow-Origin",
-      "Access-Control-Allow-Credentials",
-      "Access-Control-Allow-Methods",
-      "Access-Control-Allow-Headers"
-    ],
-    preflightContinue: false,
-    optionsSuccessStatus: 204
+    origin : allowOrigins,
+    credentials : true,
 }));
-  
 
 
 // Handle preflight requests for all routes
