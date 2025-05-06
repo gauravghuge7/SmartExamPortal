6ea2341711fbe91862cb8f48994804cb8dfe59b9
gaurav ashok ghuge <guduughuge7@gmail.com>
2025-05-06 17:40:00 +0530

    allow cookies to the secure oages

--- server/src/app.js
+++ server/src/app.js
@@ -16,16 +16,45 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-const allowOrigins = [
-    "http://localhost:3000",
-    "http://localhost:8080",
-    process.env.CLIENT_URL,
-]
+const allowedOrigins = process.env.CLIENT_URL;
+
 
 app.use(cors({
-    origin : allowOrigins,
-    credentials : true,
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Allow cookies
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Accept",
+    "Origin",
+    "X-Requested-With",
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Credentials",
+    "Access-Control-Allow-Methods",
+    "Access-Control-Allow-Headers"
+  ],
+  exposedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Credentials",
+    "Access-Control-Allow-Methods",
+    "Access-Control-Allow-Headers"
+  ],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
+
 
 
 // Handle preflight requests for all routes
